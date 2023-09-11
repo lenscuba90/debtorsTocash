f833ffa72403749709e95955af220c8138be5903
lenscuba90 <lenscuba90@hotmail.com>
2023-09-11 14:46:09 +1000

    install.sh update

--- install.sh
+++ install.sh
@@ -1,65 +1,56 @@
- #!/bin/bash
+#!/bin/bash
 
-# Step 1: Update & Upgrade System Packages
-now=$(date +'%H:%M:%S')
-echo -e "\e[1mStep 1 $now : Updating System Packages\e[0m"
-sudo apt update -y
-sudo apt upgrade -y
+echo "Step 1 $(date +"%T") : Updating System Packages"
+sudo apt update && sudo apt upgrade -y
 
-# Step 2: Install Git
-now=$(date +'%H:%M:%S')
-echo -e "\e[1mStep 2 $now : Installing Git\e[0m"
-sudo apt install git -y
+echo "Step 2 $(date +"%T") : Installing Git"
+sudo apt install -y git
 
-# Step 3: Install Nginx
-now=$(date +'%H:%M:%S')
-echo -e "\e[1mStep 3 $now : Installing Nginx\e[0m"
-sudo apt install nginx -y
+echo "Step 3 $(date +"%T") : Installing Nginx"
+sudo apt install -y nginx
 
-# Step 4: Install Docker
-now=$(date +'%H:%M:%S')
-echo -e "\e[1mStep 4 $now : Installing Docker\e[0m"
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update -y
-sudo apt install docker-ce -y
+echo "Step 4 $(date +"%T") : Installing Docker"
+sudo apt update && \
+sudo apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
 
-# Step 5: Install Certbot
-now=$(date +'%H:%M:%S')
-echo -e "\e[1mStep 5 $now : Installing Certbot\e[0m"
-sudo apt install certbot python3-certbot-nginx -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-# Step 6: Install Docker Compose
-now=$(date +'%H:%M:%S')
-echo -e "\e[1mStep 6 $now : Installing Docker Compose\e[0m"
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Step 7: Install Node.js
-now=$(date +'%H:%M:%S')
-echo -e "\e[1mStep 7 $now : Installing Node.js\e[0m"
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get update
-sudo apt-get install nodejs -y 
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io -y
 
-# Step 8: Node.js installation includes npm, moving to the next step.
-now=$(date +'%H:%M:%S')
-echo -e "\e[1mStep 8 $now : Node.js installation includes npm, moving to the next step.\e[0m"
+echo "Step 5 $(date +"%T") : Installing Certbot"
+sudo apt install -y certbot python3-certbot-nginx
 
-# Step 9: Set Up Docker User Group and User
-now=$(date +'%H:%M:%S')
-echo -e "\e[1mStep 9 $now : Setting Up Docker User Group and User\e[0m"
+echo "Step 6 $(date +"%T") : Installing Docker Compose"
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+echo "Step 7 $(date +"%T") : Installing NVM"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+
+# Source the NVM environment variables
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+echo "Step 8 $(date +"%T") : Installing Node.js using NVM"
+nvm install --lts  # This will install the latest LTS version of Node.js
+nvm use --lts
+
+echo "Step 9 $(date +"%T") : Setting Up Docker User Group and User"
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
-# Step 10: Install NPM Packages
-now=$(date +'%H:%M:%S')
-echo -e "\e[1mStep 10 $now : Installing Project Specific NPM Packages\e[0m"
-cd /home/len/debtorsTocash.com
-npm install @apollo/client @apollo/server body-parser concurrently cors express graphql graphql-http mongoose react react-dom react-router-dom typescript
+echo "Step 10 $(date +"%T") : Installing Project Specific NPM Packages"
+npm install
+
+echo "Script execution completed."
